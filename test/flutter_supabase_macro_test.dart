896d8b97740f2b0617e4eea05c1b52a1895e5ffd
Thomas Ferreira <thomas.devapps@gmail.com>
2024-10-08 17:53:48 +0200

    remove example

--- test/flutter_supabase_macro_test.dart
+++ test/flutter_supabase_macro_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:flutter_supabase_macro/flutter_supabase_macro.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+@FlutterSupabaseMacro()
+class TestMacro {
+  final String id;
+}
+
+void main() {
+  test('a', () {
+    final test = TestMacro(id: 'daa');
+  });
+}
