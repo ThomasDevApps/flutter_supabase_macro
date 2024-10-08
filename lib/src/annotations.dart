351ed2d9b99e920ccec2efdbca34343fbdf338f5
Thomas Ferreira <thomas.devapps@gmail.com>
2024-10-08 16:36:41 +0200

    setup

---
+++ lib/src/annotations.dart
@@ -0,0 +1,15 @@
+library;
+
+import 'dart:async';
+
+import 'package:macros/macros.dart';
+
+macro class FlutterSupabaseMacro implements ClassDeclarationsMacro {
+  const FlutterSupabaseMacro();
+
+  @override
+  FutureOr<void> buildDeclarationsForClass(ClassDeclaration clazz, MemberDeclarationBuilder builder,) {
+    // TODO: implement buildDeclarationsForClass
+    throw UnimplementedError();
+  }
+}
