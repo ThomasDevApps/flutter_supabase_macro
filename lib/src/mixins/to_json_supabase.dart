16daf00a89baa74e453f15da64a256b2aa28a8b8
Thomas Ferreira <thomas.devapps@gmail.com>
2024-10-10 14:56:16 +0200

    chore: continue toJsonSupabase

--- lib/src/mixins/to_json_supabase.dart
+++ lib/src/mixins/to_json_supabase.dart
@@ -1,16 +1,23 @@
 part of '../supabase_macro.dart';
 
 mixin _ToJsonSupabase on _Shared {
-  Future<void> _buildToJson(
+  Future<void> _buildToJsonSupabase(
     ClassDeclaration clazz,
     TypeDefinitionBuilder typeBuilder,
     _SharedIntrospectionData introspectionData,
   ) async {
     final methods = await typeBuilder.methodsOf(clazz);
     final toJsonSupabase = methods.firstWhereOrNull(
-      (m) => m.identifier.name == 'toJsonSupabase',
+      (m) => m.identifier.name == _toJsonMethodName,
     );
     await _initialCheck(toJsonSupabase, typeBuilder, introspectionData);
+
+    final superclassHasToJson =
+        await _checkSuperclassHasToJson(introspectionData, typeBuilder);
+    if (superclassHasToJson == null) return;
+
+    final parts = _createParts(introspectionData,
+        superclassHasToJson: superclassHasToJson);
   }
 
   /// Returns void if [toJsonSupabase] not exist.
@@ -58,5 +65,92 @@ mixin _ToJsonSupabase on _Shared {
       return false;
     }
     return true;
+  }
+
+  /// Check if the superclass (if exist) has a [_toJsonMethodName].
+  Future<bool?> _checkSuperclassHasToJson(
+    _SharedIntrospectionData introspectionData,
+    DefinitionBuilder builder,
+  ) async {
+    bool superclassHasToJson = false;
+    final superclassDeclaration = introspectionData.superclass;
+    final superClassIsObject =
+        superclassDeclaration?.isExactly('Object', _dartCore);
+    if (superclassDeclaration != null && !superClassIsObject!) {
+      final superclassMethods = await builder.methodsOf(superclassDeclaration);
+      for (final superMethod in superclassMethods) {
+        if (superMethod.identifier.name == _toJsonMethodName) {
+          final jsonMethodIsValid =
+              await _checkValidToJson(superMethod, introspectionData, builder);
+          if (!jsonMethodIsValid) return null;
+          superclassHasToJson = true;
+          break;
+        }
+      }
+      // If the superclass has not a toJsonSupabase method
+      if (!superclassHasToJson) {
+        builder.report(
+          Diagnostic(
+            DiagnosticMessage(
+              'Serialization of classes that extend other classes is only '
+              'supported if those classes have a valid '
+              '`Map<String, Object?> toJson()` method.',
+              target: introspectionData.clazz.superclass?.asDiagnosticTarget,
+            ),
+            Severity.error,
+          ),
+        );
+        return null;
+      }
+    }
+    return superclassHasToJson;
+  }
+
+  // TODO à doc
+  List<Object> _createParts(
+    _SharedIntrospectionData introspectionData, {
+    required bool superclassHasToJson,
+  }) {
+    return [
+      '{\n    final json = ',
+      if (superclassHasToJson)
+        'super.toJson()'
+      else ...[
+        '<',
+        introspectionData.stringCode,
+        ', ',
+        introspectionData.objectCode.asNullable,
+        '>{}',
+      ],
+      ';\n    ',
+    ];
+  }
+
+  Future<Code> addEntryForField(FieldDeclaration field) async {
+    final parts = <Object>[];
+    final doNullCheck = field.type.isNullable;
+    if (doNullCheck) {
+      parts.addAll([
+        'if (',
+        field.identifier,
+        ' != null) {\n      ',
+      ]);
+    }
+    parts.addAll(["json[r'", field.identifier.name, "'] = ", await _con]);
+  }
+
+  Future<Code> _convertTypeToJson(
+    TypeAnnotation rawType,
+    Code valueReference,
+    DefinitionBuilder builder,
+    _SharedIntrospectionData introspectionData,
+  ) async {
+    final type = _checkNamedType(rawType, builder);
+    if (type == null) {
+      return RawCode.fromString(
+        "throw 'Unable to serialize type ${rawType.code.debugString}'",
+      );
+    }
+    // TODO à continuer
   }
 }
