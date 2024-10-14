96515acbd15d2ad4e17b037c2d5eb041398db1d9
Thomas Ferreira <thomas.devapps@gmail.com>
2024-10-14 14:27:24 +0200

    chore: refractor

--- lib/flutter_supabase_macro.dart
+++ lib/flutter_supabase_macro.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:macros/macros.dart';
 
 part 'src/extensions/iterable_extension.dart';
+part 'src/extensions/named_type_annotation_extension.dart';
 part 'src/mixins/shared.dart';
 part 'src/mixins/to_json_supabase.dart';
 part 'src/models/shared_introspection_data.dart';
@@ -82,43 +83,5 @@ extension on Code {
           buffer.write(part);
       }
     }
-  }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-extension on NamedTypeAnnotation {
-  /// Follows the declaration of this type through any type aliases, until it
-  /// reaches a [ClassDeclaration], or returns null if it does not bottom out on
-  /// a class.
-  Future<ClassDeclaration?> classDeclaration(DefinitionBuilder builder) async {
-    var typeDecl = await builder.typeDeclarationOf(identifier);
-    while (typeDecl is TypeAliasDeclaration) {
-      final aliasedType = typeDecl.aliasedType;
-      if (aliasedType is! NamedTypeAnnotation) {
-        builder.report(
-          Diagnostic(
-            DiagnosticMessage(
-                'Only fields with named types are allowed on serializable '
-                    'classes',
-                target: asDiagnosticTarget,
-            ),
-          Severity.error,
-          ),
-        );
-        return null;
-      }
-      typeDecl = await builder.typeDeclarationOf(aliasedType.identifier);
-    }
-    if (typeDecl is! ClassDeclaration) {
-      builder.report(Diagnostic(
-          DiagnosticMessage(
-              'Only classes are supported as field types for serializable '
-                  'classes',
-              target: asDiagnosticTarget),
-          Severity.error));
-      return null;
-    }
-    return typeDecl;
   }
 }
